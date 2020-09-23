318aec27e597be3db82e25357e6f7a848a461d6b
Lee Hangsen <44113536+ThyToast@users.noreply.github.com>
2020-09-23 23:04:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,49 +28,49 @@ def get_data():
         return df
     
 # CSS Theme 
-# st.markdown(
-#     """
-# <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+st.markdown(
+    """
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 
-# <style>
-# .reportview-container .markdown-text-container {
-#     font-family: 'Open Sans', sans-serif;
-# }
-# .sidebar .sidebar-content {
-#     background-image: linear-gradient(#303039,#303039);
-#     color: #303039;
-# }
-# .Widget>label {
-#     color: white;
-#     font-family: 'Open Sans', sans-serif;
-# }
-# [class^="st-b"]  {
-#     color: white;
-#     font-family: 'Open Sans', sans-serif;
-# }
-# .st-bb {
-#     background-color: transparent;
-# }
-# .st-cz {
-#     fill: white;
-# }
-# .btn-outline-secondary{
-#     border-color: #e83e8c;
-#     color: #e83e8c;
-# }
-# .st-at {
-#     background-color: #303039;
-# }
-# .st-df {
-#     background-color: #303039;
-# }
-# footer {
-#     font-family: 'Open Sans', sans-serif;
-# }
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
+<style>
+.reportview-container .markdown-text-container {
+    font-family: 'Open Sans', sans-serif;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#303039,#303039);
+    color: #303039;
+}
+.Widget>label {
+    color: white;
+    font-family: 'Open Sans', sans-serif;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: 'Open Sans', sans-serif;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-cz {
+    fill: white;
+}
+.btn-outline-secondary{
+    border-color: #e83e8c;
+    color: #e83e8c;
+}
+.st-at {
+    background-color: #303039;
+}
+.st-df {
+    background-color: #303039;
+}
+footer {
+    font-family: 'Open Sans', sans-serif;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
 countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bonaire Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Curacao', 'Cyprus', 'Czech Republic', 'Democratic Republic of Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Faeroe Islands', 'Falkland Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'International', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'San Marino', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tanzania', 'Thailand', 'Timor', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turks and Caicos Islands', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Virgin Islands', 'Uruguay', 'Uzbekistan', 'Vatican', 'Venezuela', 'Vietnam', 'Zambia', 'Zimbabwe']
 
